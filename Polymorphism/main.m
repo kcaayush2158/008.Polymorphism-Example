d870de2b6a2ab6321dbf181e4fd8dd9fb64ac9e7
Aayush K.C <aayushkc2158@gmail.com>
2020-05-08 18:03:53 +0545

    Initial Commit

---
+++ Polymorphism/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Polymorphism
+//
+//  Created by Aayush Kc on 5/8/20.
+//  Copyright © 2020 Aayush Kc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
